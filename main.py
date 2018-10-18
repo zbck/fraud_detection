70db816bd743eb209ddeb31c9c88aea4e689c6d2
leon <37694395+zbck@users.noreply.github.com>
2018-10-18 12:57:28 +0200

    update TODO

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Feature_selection import Feature_selection
 from Random_forest import Random_forest
 from Metrics import Metrics
 
-# TODO : strore the different metrics for each loop
+# TODO : store the different metrics for each loop
 # TODO : Correct warnings
 # TODO : Add logs
 
